cbb43e2902278d495d4980d2741af396c343939f
Michael Dijamco <mdd233@cornell.edu>
2016-06-27 16:40:25 -0400

    feat: Routed album detail to music player

--- app/src/main/java/com/example/android/musicplayer/AlbumDetailActivity.java
+++ app/src/main/java/com/example/android/musicplayer/AlbumDetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class AlbumDetailActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class AlbumDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album_detail);
+
+        View albumSongList = (View) findViewById(R.id.album_song_list);
+        if (albumSongList != null) {
+            albumSongList.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Create a new intent to open the {@link FamilyActivity}
+                    Intent playerIntent = new Intent(AlbumDetailActivity.this, PlayerActivity.class);
+
+                    // Start the new activity
+                    startActivity(playerIntent);
+                }
+            });
+        }
     }
 }
