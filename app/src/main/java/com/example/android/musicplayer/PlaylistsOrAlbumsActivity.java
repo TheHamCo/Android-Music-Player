590c0dd422d80fc59913f2813ef91285078175c0
Michael Dijamco <mdd233@cornell.edu>
2016-06-27 16:14:57 -0400

    feat: Route to album details

--- app/src/main/java/com/example/android/musicplayer/PlaylistsOrAlbumsActivity.java
+++ app/src/main/java/com/example/android/musicplayer/PlaylistsOrAlbumsActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class PlaylistsOrAlbumsActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class PlaylistsOrAlbumsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlists_or_albums);
+
+        View playlistOrAlbum = (View)findViewById(R.id.playlist_or_album);
+        if (playlistOrAlbum != null) {
+            playlistOrAlbum.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Create a new intent to open the {@link FamilyActivity}
+                    Intent albumDetailIntent = new Intent(PlaylistsOrAlbumsActivity.this, AlbumDetailActivity.class);
+
+                    // Start the new activity
+                    startActivity(albumDetailIntent);
+                }
+            });
+        }
     }
 }
