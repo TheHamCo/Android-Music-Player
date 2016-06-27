5fdfdb0271fe287e92dc90f693e171fc068ff0e0
Michael Dijamco <mdd233@cornell.edu>
2016-06-27 17:12:21 -0400

    feat: Layout player

--- app/src/main/java/com/example/android/musicplayer/SongsActivity.java
+++ app/src/main/java/com/example/android/musicplayer/SongsActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class SongsActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class SongsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_songs);
+
+        View songList = (View)findViewById(R.id.song_list);
+        if (songList != null) {
+            songList.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent playerIntent = new Intent(SongsActivity.this, PlayerActivity.class);
+                    startActivity(playerIntent);
+                }
+            });
+        }
     }
 }
